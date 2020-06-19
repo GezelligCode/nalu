cbe91e8cce37422e5ecf74d82f1ecc2ccfa8b8b6
OctopodeCode <52639631+OctopodeCode@users.noreply.github.com>
2020-06-18 20:59:32 -0700

    updating app with a shop page, collection preview component and collection components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
+
+// NOTE: NEED TO SETUP ROUTING TO HATS PAGE. 6/15/2020
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact={true} path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />    
+        <HomePage />
+      </Switch>
     </div>
     );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,9 +4,9 @@ import Directory from '../../components/directory/directory.component';
 
 
 
-const HomePage = () => (
+const HomePage = ({history}) => (
 <div>
-    <Directory />
+    <Directory history={history} />
 </div>
 );
 
